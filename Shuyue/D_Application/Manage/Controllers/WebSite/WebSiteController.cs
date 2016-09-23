29e0a30d161bfba8d08fa07b4cb566226d116641
home_sqy <tianfei@MYLIFE>
2016-09-23 20:30:28 +0800

    添加文件

---
+++ Shuyue/D_Application/Manage/Controllers/WebSite/WebSiteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manage.Controllers.WebSite
+{
+    public class WebSiteController : Controller
+    {
+        // GET: WebSite
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Button()
+        {
+            return View();
+        }
+        public ActionResult TextBox()
+        {
+            return View();
+        }
+    }
+}
