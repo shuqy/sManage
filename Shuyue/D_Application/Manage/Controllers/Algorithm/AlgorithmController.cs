9b1b1ecd127136647ec71770e6a6e4e916a5c01d
home_sqy <tianfei@MYLIFE>
2016-10-10 20:22:00 +0800

    添加算法页面

---
+++ Shuyue/D_Application/Manage/Controllers/Algorithm/AlgorithmController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manage.Controllers.Algorithm
+{
+    public class AlgorithmController : Controller
+    {
+        // GET: Algorithm
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shuyue/D_Application/Manage/Controllers/Algorithm/SortController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manage.Controllers.Algorithm
+{
+    public class SortController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Selection()
+        {
+            return View();
+        }
+    }
+}
