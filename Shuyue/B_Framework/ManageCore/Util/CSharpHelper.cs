4dd7de584efca56220032682acbdaab1cebf0d05
sqy <992312195@qq.com>
2016-10-11 14:33:04 +0800

    csharp添加更改代码颜色方法

--- Shuyue/B_Framework/ManageCore/Util/CSharpHelper.cs
+++ Shuyue/B_Framework/ManageCore/Util/CSharpHelper.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Core.Util
 {
     public class CSharpHelper
     {
-        public static string AddColorTag(string str)
+        /// <summary>
+        /// 更改代码颜色
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ChangeColor(string str)
         {
+            //自定义类颜色更改
+            Regex userClass = new Regex(@"class (?<className>[\s\S]*?)( |\(|<)");
+            Match classMatch = userClass.Match(str);
+            while (classMatch.Success)
+            {
+                str = str.Replace(classMatch.Groups["className"].ToString(), "<font color=\"#00868B\">" + classMatch.Groups["className"] + "</font>");
+                classMatch = classMatch.NextMatch();
+            }
+            //保留关键字
             string[] reservedKeyword = new[]
             {
                 "abstract","event","new","struct","as","explicit","null","switch","base","extern","object",
@@ -17,18 +33,59 @@ namespace Core.Util
                 "true","byte","fixed","override","try","case","float","params","typeof","catch","for","private",
                 "uint","char","foreach","protected","ulong","checked","goto","public","unchecked","class","if",
                 "readonly","unsafe","const","implicit","ref","ushort","continue","in","return","using","decimal",
-                "int","sbyte","virtual","default","interface","sealed","volatile",
-                "delegate","internal","short","void","do","is","sizeof","while",
+                "int","sbyte","virtual","default","interface","sealed","volatile","where",
+                "delegate","internal","short","void","do","is","sizeof","while","var",
                 "double","lock","stackalloc","else","long","static","enum","namespace","string"
             };
             foreach (var keyw in reservedKeyword)
             {
                 if (str.Contains(keyw))
                 {
-                    str = str.Replace(keyw, "<font color=\"blue\">" + keyw + "</font>");
+                    Regex reg = new Regex(" ?" + keyw + " ");
+                    str = reg.Replace(str, " <font color=\"blue\">" + keyw + "</font> ");
                 }
             }
-            return "";
+            //系统类
+            string[] objClass = AllClassName(typeof(object));
+            string[] sysClass = AllClassName(typeof(object));
+            string[] className = objClass.Concat(sysClass).ToArray();
+            for (int i = 0; i < className.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(className[i]) && str.Contains(className[i]))
+                {
+                    Regex reg = new Regex("( )?" + className[i] + "( )");
+                    str = reg.Replace(str, " <font color=\"#00868B\">" + className[i] + "</font> ");
+                    Regex reg2 = new Regex("( )?" + className[i] + "(<)");
+                    str = reg2.Replace(str, " <font color=\"#00868B\">" + className[i] + "</font><");
+                    Regex reg3 = new Regex("( )?" + className[i] + @"(\()");
+                    str = reg3.Replace(str, " <font color=\"#00868B\">" + className[i] + "</font>(");
+                }
+            }
+            //泛型
+            if (str.Contains("<T>"))
+            {
+                str = str.Replace("<T>", "<<font color=\"#00868B\">T</font>>");
+                str = str.Replace("(T", "(<font color=\"#00868B\">T</font>");
+                str = str.Replace("T[", "<font color=\"#00868B\">T</font>[");
+                str = str.Replace("T ", "<font color=\"#00868B\">T</font> ");
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 获取所有系统类名称
+        /// </summary>
+        /// <returns></returns>
+        public static string[] AllClassName(Type type)
+        {
+            Assembly _Assembyle = Assembly.GetAssembly(type);
+            Type[] _TypeList = _Assembyle.GetTypes();
+            string[] classNames = new string[_TypeList.Length + 1];
+            for (int i = 0; i != _TypeList.Length; i++)
+            {
+                classNames[i] = _TypeList[i].Name;
+            }
+            return classNames;
         }
     }
 }
--- Shuyue/D_Application/Manage/Controllers/Algorithm/SortController.cs
+++ Shuyue/D_Application/Manage/Controllers/Algorithm/SortController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Core.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,6 +16,22 @@ namespace Manage.Controllers.Algorithm
         }
         public ActionResult Selection()
         {
+            StringBuilder selectionHtml = new StringBuilder();
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">public class SortTemp<T> where T : IComparable<T> {</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:4em;\">public static bool Less(T a, T b) {</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:6em;\">return a.CompareTo(b) < 0;</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:4em;\">}</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:4em;\">public static void Exch(T[] arr, int i, int j) {</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:6em;\">var temp = arr[i];</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:6em;\">arr[i] = arr[j];</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:6em;\">arr[j] = temp;</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:4em;\">}</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">}</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">public class SortController : Controller </p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">public ActionResult Index()</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">StringBuilder selectionHtml = new StringBuilder();</p>");
+            selectionHtml.AppendLine("<p style=\"text-indent:2em;\">return View();</p>");
+            ViewBag.SelectionSortStr = CSharpHelper.ChangeColor(selectionHtml.ToString());
             return View();
         }
     }
