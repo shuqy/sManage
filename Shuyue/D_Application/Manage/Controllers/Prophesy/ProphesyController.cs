e13ff2888a7c05e0a77eb29bde42e67cef7a7f86
sqy <992312195@qq.com>
2017-01-11 19:01:22 +0800

    添加预言controller

---
+++ Shuyue/D_Application/Manage/Controllers/Prophesy/ProphesyController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manage.Controllers.Prophesy
+{
+    public class ProphesyController : ControllerBase
+    {
+        // GET: Prophesy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 更新行业信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UpdateIndustry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult UpdateIndustry(string path)
+        {
+            DataTable dt = NPOIHelper.ImportExceltoDt(path);
+            
+            return null;
+        }
+    }
+}
