2c7565da769a676bf86a70e8ae9b8595779c2c58
sqy <992312195@qq.com>
2017-01-12 17:02:19 +0800

    更名

--- Shuyue/A_Model/Model/Extend/T_Stock.cs
+++ Shuyue/A_Model/Model/Extend/T_Stock.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model.Extend
+namespace Model
 {
     public partial class T_Stock
     {
--- Shuyue/D_Application/Manage/Controllers/Prophesy/ProphesyController.cs
+++ Shuyue/D_Application/Manage/Controllers/Prophesy/ProphesyController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities;
 using Core.Util;
+using Core.Utilities;
 using Model;
 using System;
 using System.Collections.Generic;
@@ -22,21 +23,65 @@ namespace Manage.Controllers.Prophesy
         /// 更新行业信息
         /// </summary>
         /// <returns></returns>
-        public ActionResult UpdateIndustry()
+        public ActionResult UpBaseData()
         {
             return View();
         }
 
+        /// <summary>
+        /// 更新数据信息
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         [HttpPost]
-        public JsonResult UpdateIndustry(string path)
+        public JsonResult UpBaseData(string path)
         {
+            var db = Core.AppContext.Current.StockDbContext;
+            //读取Excel中的数据
             DataTable dt = NPOIHelper.ImportExceltoDt(Server.MapPath(path));
             List<T_Stock> stockList = ModelHelper.ExcelFillModelList<T_Stock>(dt).ToList();
-            foreach(var item in stockList)
+            //行业数据/新增行业列表
+            List<T_Industry> industryList = db.T_Industry.ToList(), newIndustryList = new List<T_Industry>();
+            //股票信息列表/新增股票列表
+            List<T_Stock> currentStockList = db.T_Stock.ToList(), newStockList = new List<T_Stock>();
+            foreach (var item in stockList)
             {
-
+                if (!industryList.Any(i => i.Name.Equals(item.IndustryName)) && !newIndustryList.Any(i => i.Name.Equals(item.IndustryName)))
+                    newIndustryList.Add(new T_Industry { Name = item.IndustryName });
+                if (!currentStockList.Any(s => s.FullCode == item.FullCode))
+                {
+                    T_Stock stock = new T_Stock
+                    {
+                        FullCode = item.FullCode,
+                        StockName = item.StockName,
+                        TotalAmount = item.TotalAmount,
+                        TotalMarketValue = item.TotalMarketValue,
+                        CirculationMarketValue = item.CirculationMarketValue,
+                        CreatedOn = DateTime.Now,
+                        Deleted = false,
+                        StockCode = item.FullCode.Substring(2),
+                        PyAbbre = PyHelper.GetFirst(item.StockName),
+                        PyFullName = PyHelper.Get(item.StockName),
+                        IndustryName = item.IndustryName,
+                    };
+                    newStockList.Add(stock);
+                }
             }
-            return Json(new JsonData { Code = Core.Enum.ResultCode.OK }, JsonRequestBehavior.DenyGet);
+            int count1 = newIndustryList.Count(), count2 = newStockList.Count();
+            if (newIndustryList.Count() > 0)
+            {
+                db.T_Industry.AddRange(newIndustryList);
+                db.SaveChanges();
+            }
+            if (newStockList.Count() > 0)
+            {
+                var currentIndustryList = db.T_Industry.ToList();
+                //获取行业id
+                newStockList.ForEach(s => s.Industry = (currentIndustryList.FirstOrDefault(i => i.Name.Equals(s.IndustryName)) ?? new T_Industry()).Id);
+                db.T_Stock.AddRange(newStockList);
+                db.SaveChanges();
+            }
+            return Json(new JsonData { Code = Core.Enum.ResultCode.OK, Data = new { count1 = count1, count2 = count2 } }, JsonRequestBehavior.DenyGet);
         }
     }
 }
