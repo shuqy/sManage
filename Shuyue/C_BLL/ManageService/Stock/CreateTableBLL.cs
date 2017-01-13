540afa936fa4ac121933308382278c52715c284f
sqy <992312195@qq.com>
2017-01-13 17:01:46 +0800

    添加stockbll

---
+++ Shuyue/C_BLL/ManageService/Stock/CreateTableBLL.cs
@@ -0,0 +1,57 @@
+﻿using Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageService.Stock
+{
+    public class CreateTableBLL
+    {
+        /// <summary>
+        /// 判断表是否存在
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public static bool IsExistTable(string tableName)
+        {
+            string dbStr = string.Format("select 1 from sysobjects where id = object_id('{0}') and type ='U'", tableName);
+            DataSet ds = SqlHelper.ExecuteDataSet(ConfigHelper.GetConnStr("StockConn"), CommandType.Text, dbStr);
+            return ds.Tables[0].Rows.Count != 0;
+        }
+
+        /// <summary>
+        /// 创建股票表
+        /// </summary>
+        /// <param name="stockCode">股票代码</param>
+        /// <returns></returns>
+        public static bool CreateStockTableOrNot(string stockCode)
+        {
+            if (IsExistTable(string.Format("T_TransactionRecord_{0}", stockCode))) return false;
+            StringBuilder sqlsb = new StringBuilder();
+            //sqlsb.Append("using SQY_Stock ");
+            sqlsb.Append(string.Format("create table T_TransactionRecord_{0}(", stockCode));
+            sqlsb.Append("Id int identity(1,1) primary key,");
+            sqlsb.Append("StockCode varchar(50) not null,");
+            sqlsb.Append("TradingDate datetime not null,");
+            sqlsb.Append("[Open] decimal(18,4) not null,");
+            sqlsb.Append("[High] decimal(18,4) not null,");
+            sqlsb.Append("[Low] decimal(18,4) not null,");
+            sqlsb.Append("[Close] decimal(18,4) not null,");
+            sqlsb.Append("[Rose] decimal(18,4) not null,");
+            sqlsb.Append("[Amplitude] decimal(18,4) not null,");
+            sqlsb.Append("[Hands] int not null,");
+            sqlsb.Append("[Amount] decimal(18,4) not null,");
+            sqlsb.Append("[Turnover] decimal(18,4) not null,");
+            sqlsb.Append("[VOLAMOUNT] int not null,");
+            sqlsb.Append("[CreatedOn] datetime not null default getdate(),");
+            sqlsb.Append("[UpdatedOn] datetime null,");
+            sqlsb.Append("[Deleted] bit not null default 0,");
+            sqlsb.Append(") ");
+            sqlsb.Append(string.Format("CREATE INDEX Ix_T_TransactionRecord_{0}_Date ON T_TransactionRecord_{0}(TradingDate)", stockCode));
+            return SqlHelper.ExecuteNonQuery(ConfigHelper.GetConnStr("StockConn"), CommandType.Text, sqlsb.ToString()) > 0;
+        }
+    }
+}
---
+++ Shuyue/C_BLL/ManageService/Stock/StockBLL.cs
@@ -0,0 +1,72 @@
+﻿using Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageService.Stock
+{
+    public class StockBLL
+    {
+        /// <summary>
+        /// 更新股票交易记录
+        /// </summary>
+        /// <param name="stockCode"></param>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public int UpSingleStockData(string stockCode, DataTable dt)
+        {
+            int count = 0, runCount = 500;
+            //检查表是否存在，不存在则创建
+            CreateTableBLL.CreateStockTableOrNot(stockCode);
+            int begin = 0, end = dt.Rows.Count >= runCount ? runCount : dt.Rows.Count;
+            for (int i = 0; i < dt.Rows.Count / runCount + 1; i++)
+            {
+                string sql = getSignleStockInsertStr(stockCode, dt, begin, end);
+                begin = end + 1;
+                end = dt.Rows.Count >= end + runCount ? end + runCount : dt.Rows.Count - 1;
+                count += SqlHelper.ExecuteNonQuery(ConfigHelper.GetConnStr("StockConn"), CommandType.Text, sql.ToString());
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// 获取插入sql语句，分段插入，防止用时过长，导致超时
+        /// </summary>
+        /// <param name="stockCode"></param>
+        /// <param name="dt"></param>
+        /// <param name="begin"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        private string getSignleStockInsertStr(string stockCode, DataTable dt, int begin, int end)
+        {
+            StringBuilder sqlsb = new StringBuilder();
+            if (begin == 0)
+                sqlsb.Append(string.Format("delete T_TransactionRecord_{0};", stockCode));
+            sqlsb.Append(string.Format("insert into T_TransactionRecord_{0} (StockCode,TradingDate,[Open],[High],[Low],[Close],[Rose],[Amplitude],[Hands],[Amount],[Turnover],[VOLAMOUNT])", stockCode));
+            for (int i = begin; i <= end; i++)
+            {
+                if (string.IsNullOrEmpty(dt.Rows[i][0].ToString())) continue;
+                DateTime tradingDate = Convert.ToDateTime(dt.Rows[i][0].ToString().Split(',')[0]);
+                decimal open = 0, high = 0, low = 0, close = 0, rose = 0, amplitude = 0, amount = 0, turnover = 0;
+                int hands = 0, VOLAMOUNT = 0;
+                decimal.TryParse(dt.Rows[i][1].ToString(), out open);
+                decimal.TryParse(dt.Rows[i][2].ToString(), out high);
+                decimal.TryParse(dt.Rows[i][3].ToString(), out low);
+                decimal.TryParse(dt.Rows[i][4].ToString(), out close);
+                decimal.TryParse(dt.Rows[i][5].ToString(), out rose);
+                decimal.TryParse(dt.Rows[i][6].ToString(), out amplitude);
+                int.TryParse(dt.Rows[i][7].ToString(), out hands);
+                decimal.TryParse(dt.Rows[i][8].ToString(), out amount);
+                decimal.TryParse(dt.Rows[i][9].ToString(), out turnover);
+                int.TryParse(dt.Rows[i][10].ToString(), out VOLAMOUNT);
+                sqlsb.Append(string.Format(" select '{0}','{1}',{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}",
+                    stockCode, tradingDate, open, high, low, close, rose, amplitude, hands, amount, turnover, VOLAMOUNT));
+                if (i != end) sqlsb.Append(" union ");
+            }
+            return sqlsb.ToString();
+        }
+    }
+}
