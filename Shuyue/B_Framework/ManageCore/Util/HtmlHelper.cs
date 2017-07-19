e2d43bc1341cedb8cef1d43e862eb824f1ef2325
sqy <992312195@qq.com>
2017-07-19 19:53:21 +0800

    s

---
+++ Shuyue/B_Framework/ManageCore/Util/HtmlHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Core.Util
+{
+    public class HtmlHelper
+    {
+        /// <summary>
+        /// 获得字符串中开始和结束字符串中间得值
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="s">开始</param>
+        /// <param name="e">结束</param>
+        /// <returns></returns>
+        public static string GetValue(string str, string s, string e)
+        {
+            string regexStr = "";
+            if (s != string.Empty && e != string.Empty)
+            {
+                regexStr = "(?<=(" + s + "))[.\\s\\S]*?(?=(" + e + "))";
+            }
+            else
+            {
+                regexStr = "(?<=(" + s + "))[.\\s\\S]*";
+            }
+            Regex rg = new Regex(regexStr, RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            return rg.Match(str).Value;
+        }
+
+        /// <summary>
+        /// 获得所有字符串中开始和结束字符串中间得值
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="begin"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static List<string> GetValueList(string str, string begin, string end)
+        {
+            string regexStr = "(?<=(" + begin + "))[.\\s\\S]*?(?=(" + end + "))";
+            Regex rg = new Regex(regexStr, RegexOptions.Multiline | RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            Match m = rg.Match(str);
+            List<string> matchRes = new List<string>();
+            while (m.Success)
+            {
+                matchRes.Add(m.Value);
+                m = m.NextMatch();
+            }
+            return matchRes;
+        }
+    }
+}
---
+++ Shuyue/C_BLL/FFService/Stock/StockDataBLL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Util;
+using Core.Enum;
+using Model.ViewModel.ff;
+using System.Text.RegularExpressions;
+
+namespace FFService.Stock
+{
+    public class StockDataBLL
+    {
+        /// <summary>
+        /// 更新所有股票名称代码
+        /// </summary>
+        public int UpdateStocks()
+        {
+            string stocksListURL = "http://quote.eastmoney.com/stocklist.html";
+            HttpHelper httpHelper = new HttpHelper();
+            HttpItem item = new HttpItem
+            {
+                URL = stocksListURL,
+                Method = "get",
+            };
+            HttpResult result = httpHelper.GetHtml(item);
+            string html = "";
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                html = result.Html;
+            }
+            Regex regex = new Regex("<a target=\"_blank\".*?html\">(?<name>[^<]*)");
+            Match match = regex.Match(html);
+            List<stocks> stocks = new List<stocks>();
+            while (match.Success)
+            {
+                string s = match.Groups["name"].Value;
+                string stockName = s.Substring(0, s.IndexOf('('));
+                string stockCode = s.Substring(s.IndexOf('(') + 1, 6);
+                match = match.NextMatch();
+                if (!stockCode.StartsWith("0") && !stockCode.StartsWith("3") && !stockCode.StartsWith("6")) continue;
+                stocks.Add(new stocks
+                {
+                    name = stockName,
+                    code = stockCode,
+                    fullPy = PyHelper.Get(stockName),
+                    pyAbbre = PyHelper.GetFirst(stockName)
+                });
+            }
+            DapperMySql mysql = new DapperMySql(DbConnEnum.ff);
+            var stockList = mysql.Query<stocks>("select * from stocks;");
+            stocks = stocks.Where(s => !stockList.Any(r => r.code == s.code)).ToList();
+            int count = 0;
+            if (stocks.Any())
+            {
+                count = mysql.InsertMultiple("insert stocks (name,code,fullPy,pyAbbre) values (@name,@code,@fullPy,@pyAbbre);", stocks);
+            }
+            return count;
+        }
+    }
+}
