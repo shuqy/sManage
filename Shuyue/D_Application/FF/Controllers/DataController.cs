3c203795d1f6132c5c19e9482aaf1af44c6c2fd6
sqy <992312195@qq.com>
2017-07-19 19:52:44 +0800

    s

---
+++ Shuyue/D_Application/FF/Controllers/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FFService.Stock;
+
+namespace FF.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            StockDataBLL sbll = new StockDataBLL();
+            int count = sbll.UpdateStocks();
+            return View();
+        }
+    }
+}
